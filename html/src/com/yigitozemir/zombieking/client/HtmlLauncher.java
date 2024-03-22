5df09b742d0759c67d4b886a17a0c16995410e4a
YIGIT OZDEMIR <yozdemir@tr.ibm.com>
2024-03-22 21:27:19 +0300

    first commit

---
+++ html/src/com/yigitozemir/zombieking/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.yigitozemir.zombieking.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.yigitozemir.zombieking.ZombieKingGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ZombieKingGame();
+        }
+}
