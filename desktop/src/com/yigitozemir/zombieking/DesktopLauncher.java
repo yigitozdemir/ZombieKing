f91266e0e17902661eb56de9f44ccaef5818b96c
YIGIT OZDEMIR <yozdemir@tr.ibm.com>
2024-03-23 09:06:39 +0300

    window size set to 1280x960

--- desktop/src/com/yigitozemir/zombieking/DesktopLauncher.java
+++ desktop/src/com/yigitozemir/zombieking/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("ZombieKing");
+		config.setWindowedMode(1280, 960);
 		new Lwjgl3Application(new ZombieKingGame(), config);
 	}
 }
